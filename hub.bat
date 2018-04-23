4d34753becdd65704b415214205daffe281f3947
IgorMorozovv <igor.morozzovv@gmail.com>
2018-04-23 17:08:40 +0300

    2.3.1

---
+++ hub.bat
@@ -0,0 +1,2 @@
+cd /d "D:\For tests"
+java -jar selenium-server-standalone-3.11.0.jar -role hub -port 4441
---
+++ node.bat
@@ -0,0 +1,2 @@
+cd /d "D:\For tests"
+java -jar selenium-server-standalone-3.11.0.jar -role node -hub http://localhost:4441/grid/register -port 5556 
